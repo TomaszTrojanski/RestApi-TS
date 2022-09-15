d24fb464ca5a9a0fdf27d5b46152a9b3f46a1c53
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-09-15 11:20:24 +0200

    /archaizmy

--- src/routes/caliber.ts
+++ src/routes/caliber.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import controller from '../controllers/caliber';
-//import { Schemas, ValidateJoi } from '../middleware/Joi';
 
 const router = express.Router();
 
@@ -9,12 +8,5 @@ router.get('/get/:caliberID', controller.readCaliber);
 router.get('/get', controller.readAll);
 router.patch('/update/:caliberID', controller.updateCaliber);
 router.delete('/delete/:caliberID', controller.deleteCaliber);
-
-/* 
-router.post('/create', ValidateJoi(Schemas.caliber.create), controller.createCaliber);
-router.get('/get/:caliberID', controller.readCaliber);
-router.get('/get', controller.readAll);
-router.patch('/update/:caliberID', ValidateJoi(Schemas.author.update), controller.updateCaliber);
-router.delete('/delete/:caliberID', controller.deleteCaliber); */
 
 export = router;
